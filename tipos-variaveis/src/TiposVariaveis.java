2225c263d04df9a764641a527c4128345ad750eb
fellpsdev <felipeffjcontato@gmail.com>
2024-07-01 19:15:23 -0300

    exerciciosIniciais

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int numeroInteiro = 5;
+        numeroInteiro = 10;
+        System.out.println(numeroInteiro);
+
+        final double VALOR_PI = 3.14159;
+        System.out.println(VALOR_PI);
     }
 }
